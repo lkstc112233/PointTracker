bc067faff978a1073bf7d73a28b1fdc79552db8d
Photon cat <lkstc112233@users.noreply.github.com>
2019-12-13 22:33:50 -0800

    Initialize the welcome activity

--- app/src/main/java/com/photoncat/pointtracker/WelcomeActivity.java
+++ app/src/main/java/com/photoncat/pointtracker/WelcomeActivity.java
@@ -2,6 +2,8 @@ package com.photoncat.pointtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        EditText playerCount = findViewById(R.id.players_count);
+        EditText initialPoint = findViewById(R.id.initial_points);
+
+        Button startGame = findViewById(R.id.start_game);
+        startGame.setOnClickListener(e->{});
     }
 }
