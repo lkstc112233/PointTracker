a640518f74954a0edbe8cd16b8c2085d4dfe6930
Photon cat <lkstc112233@users.noreply.github.com>
2019-12-14 00:07:46 -0800

    Fix an issue where incorrect operations are made.

--- app/src/main/java/com/photoncat/pointtracker/PlayerListAdapter.java
+++ app/src/main/java/com/photoncat/pointtracker/PlayerListAdapter.java
@@ -30,20 +30,18 @@ public class PlayerListAdapter extends ArrayAdapter<Player> {
         if (convertView == null) {
             LayoutInflater inflater = ((Activity) context).getLayoutInflater();
             convertView = inflater.inflate(layoutResourceId, parent, false);
-            holder = new PlayerHolder();
-            holder.player = items.get(position);
-            holder.checkOrCallButton = convertView.findViewById(R.id.check_or_call);
-            holder.checkOrCallButton.setTag(position);
-            holder.raiseButton = convertView.findViewById(R.id.raise);
-            holder.raiseButton.setTag(position);
-            holder.foldButton = convertView.findViewById(R.id.fold);
-            holder.foldButton.setTag(position);
-            holder.name = convertView.findViewById(R.id.player_name);
-            holder.value = convertView.findViewById(R.id.current_point);
-            convertView.setTag(holder);
-        } else {
-            holder = (PlayerHolder) convertView.getTag();
         }
+        holder = new PlayerHolder();
+        holder.player = items.get(position);
+        holder.checkOrCallButton = convertView.findViewById(R.id.check_or_call);
+        holder.checkOrCallButton.setTag(position);
+        holder.raiseButton = convertView.findViewById(R.id.raise);
+        holder.raiseButton.setTag(position);
+        holder.foldButton = convertView.findViewById(R.id.fold);
+        holder.foldButton.setTag(position);
+        holder.name = convertView.findViewById(R.id.player_name);
+        holder.value = convertView.findViewById(R.id.current_point);
+        convertView.setTag(holder);
 
         setupItem(holder);
         return convertView;
